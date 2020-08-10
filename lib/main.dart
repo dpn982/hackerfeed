54e156e0f0b9049af469acd37bd3703e02644e2b
Dave Nichols <dpn982@gmail.com>
2020-08-09 23:47:49 -0400

    Add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hacker_feed/state/hacker_feed_bloc.dart';
 import 'package:hacker_feed/screens/home.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider(
+          create: (context) => HackerFeedBloc(),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/state/hacker_feed_bloc.dart
@@ -0,0 +1,3 @@
+class HackerFeedBloc {
+  HackerFeedBloc();
+}
